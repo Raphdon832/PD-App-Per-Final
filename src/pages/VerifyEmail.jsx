2454d95d83e5cdec6fbec4e0d406e427548d683f
Raphdon832 <50739318+Raphdon832@users.noreply.github.com>
2025-09-17 18:20:27 +0100

    Update VerifyEmail.jsx

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -1,6 +1,7 @@
 import { useAuth } from '@/lib/auth';
 import { useEffect, useState } from 'react';
 import { sendVerification } from '@/lib/email';
+import BackButton from './auth/BackButton';
 
 export default function VerifyEmail() {
   const { user, logout } = useAuth();
@@ -26,7 +27,10 @@ export default function VerifyEmail() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-white">
       <div className="bg-white p-8 max-w-md w-full text-center">
-        <h2 className="text-xl font-normal mb-2">Verify your email</h2>
+        <div className="flex mt-1 justify-start">
+          <BackButton to="/auth" />
+        </div>
+        <h2 className="text-xl font-normal mt-5 mb-2">Verify your email</h2>
         <p className="mb-4 text-zinc-600 text-[14px] font-normal">A verification link has been sent to <b>{user.email}</b>.<br />Please check your inbox and click the link to activate your account.</p>
         <button
           className="px-4 py-2 bg-black text-white rounded-[7px] text-[12px] font-light disabled:opacity-50"
